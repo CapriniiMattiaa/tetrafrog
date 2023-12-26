809cf1c6bb496070fcf803b408c7a831f2780942
tr4v4 <nicola.travaglini3@studio.unibo.it>
2023-12-26 20:32:42 +0100

    correzione bug importo multiplo di tools.h

--- src/tools.h
+++ src/tools.h
@@ -1,6 +1,18 @@
-const int NUM_SCORES_IN_LEADERBOARD = 10;
-const int MAX_DIGIT_SCORE = 10;
-const int rows_grid = 20;
-const int columns_grid = 10;
+#ifndef NUM_SCORES_IN_LEADERBOARD
+#define NUM_SCORES_IN_LEADERBOARD 10
+#endif
+
+#ifndef MAX_DIGIT_SCORE
+#define MAX_DIGIT_SCORE 10
+#endif
+
+#ifndef rows_grid
+#define rows_grid 20
+#endif
+
+#ifndef columns_grid
+#define columns_grid 10
+#endif
+
 void selection_sort_reverted(int v[], int n);
 void swap(int &a, int &b);
