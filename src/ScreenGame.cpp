f9875676fc5ddccbee990b0b88c8a480bd3a6f2b
CapriniiMattiaa <mattia.caprini3@studio.unibo.it>
2024-01-20 18:18:57 +0100

    new version of ScreenGame. Changed and deleted metods. We need to implement the tetramino that must printed

--- src/ScreenGame.cpp
+++ src/ScreenGame.cpp
@@ -1,44 +1,17 @@
 #include "ScreenGame.hpp"
-#include "../../../../../../usr/include/c++/13/bits/algorithmfwd.h"
+using namespace std;
 
-void ScreenGame::displayGameBoard() const {
-    std::cout << "Game Board:\n";
+void ScreenGame::displayGameBoard(){
+    cout << "Game Board:\n";
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
-            if (gameBoard[i][j] == 1) {
-                std::cout << "■ ";  // Simbolo per il blocco del tetramino
+            if (gameBoard[i][j] == 1) { //controller in == 1 (full or eampty) 
+                cout << "■ ";  // print the correct and current tetramino
             } else {
-                std::cout << "  ";  // Spazio vuoto
+                cout << "  ";  // empty cout because there isn't one tetramino
             }
         }
-        std::cout << "\n";
+        cout << "\n";
     }
-    std::cout << "\n";
+    cout << "\n";
 }
-
-// Metodo per inserire un tetramino nella griglia di gioco
-void ScreenGame::placeTetramino(int tetramino[4][4], int row, int col) {
-    for (int i = 0; i < 4; ++i) {
-        for (int j = 0; j < 4; ++j) {
-            if (tetramino[i][j] == 1) {
-                // Assicurati di non superare i limiti della griglia
-                if (row + i < rows && col + j < cols) {
-                    gameBoard[row + i][col + j] = 1;
-                }
-            }
-        }
-    }
-}
-
-// Metodo per pulire una riga completata
-void ScreenGame::clearCompletedRows() {
-    for (int i = rows - 1; i >= 0; --i) {
-        if (std::all_of(gameBoard[i].begin(), gameBoard[i].end(), [](int block) { return block == 1; })) {
-            // Rimuovi la riga completata
-            gameBoard.erase(gameBoard.begin() + i);
-            // Aggiungi una nuova riga vuota in cima
-            gameBoard.insert(gameBoard.begin(), std::vector<int>(cols, 0));
-        }
-    }
-}
-
--- src/ScreenGame.hpp
+++ src/ScreenGame.hpp
@@ -1,18 +1,12 @@
 #include <iostream>
-#include <vector>
+using namespace std;
 
 class ScreenGame {
 private:
     const int rows;
     const int cols;
-    std::vector<std::vector<int>> gameBoard;
+    int gameBoard[rows][cols];
 
 public:
-    void displayGameBoard() const;
-
-    // Metodo per inserire un tetramino nella griglia di gioco
-    void placeTetramino(int tetramino[4][4], int row, int col);
-
-    // Metodo per pulire una riga completata
-    void clearCompletedRows();
+    void displayGameBoard();
 };
