9e2d8972e69f1780e303447b4c59acb1e902ed74
tr4v4 <nicola.travaglini3@studio.unibo.it>
2024-03-26 16:46:59 +0100

    ntv-debug

--- src/NextTetriminoViewer.cpp
+++ src/NextTetriminoViewer.cpp
@@ -10,34 +10,20 @@ printw("gameover:()");
 
 }
 void NextTetriminoViewer::view(Tetrimino t) {
-  /*
-  int x = 30;
-  int y = 22;
-
-  y += 1;
-  mvprintw(y, x, "Next Tetramino:");
-  y += 1;
-  mvprintw(y, x, "{|-------------|}");
-  y += 1;
-  mvprintw(y, x, "{|             |}");
-  y += 1;
-  mvprintw(y, x, "{|             |}");
-  y += 1;
-  mvprintw(y, x, "{|             |}");
-  y += 1;
-  mvprintw(y, x, "{|             |}");
-  y += 1;
-  mvprintw(y, x, "{|             |}");
-  y += 1;
-  mvprintw(y, x, "{|-------------|}");
-  */
+  start_color();
+  init_pair(1, 7, 7);
 
   for (int i = 0; i < TETRIMINO_MAX_WIDTH; i++) {
     for (int j = 0; j < TETRIMINO_MAX_HEIGHT; j++) {
       if (t.grid[i][j] == 1) {
-        mvwprintw(this->w_next_tetrimino, i + 1, j * 2 + 1, "0");
+        wattron(this->w_next_tetrimino, COLOR_PAIR(1));
+        mvwprintw(this->w_next_tetrimino, i+1, j * 2 + 1, "0");
+        mvwprintw(this->w_next_tetrimino, i+1, j * 2 + 2, "0");
+        wattroff(this->w_next_tetrimino, COLOR_PAIR(1));
       } else {
-        mvwprintw(this->w_next_tetrimino, i + 1, j * 2 + 1, " ");
+        mvwprintw(this->w_next_tetrimino, i+1, j * 2 + 1, " ");
+        mvwprintw(this->w_next_tetrimino, i+1, j * 2 + 2, " ");
+        
       }
     }
   }
